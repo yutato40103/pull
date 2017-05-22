dd3c8ae172198f8bbb51fa190e0895ed04f3a256
Shunsuke NISHINO <sr1994lu@gmail.com>
2017-05-22 17:07:56 +0900

    build(server): mock json express server

---
+++ server/app/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({
+    users: 'ユーザーです',
+  });
+});
+
+module.exports = router;
